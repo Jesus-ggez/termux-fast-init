d20b914c58b5331e1fc036f5fa6de7238b10c0e1
Jesus-ggez <165794321+Jesus-ggez@users.noreply.github.com>
2024-12-08 22:34:17 -0600

    Update add_user_gh.py
    correction in \n out of string

--- init_github/add_user_gh.py
+++ init_github/add_user_gh.py
@@ -2,7 +2,7 @@ def add_user_gh(email: str) -> None:
     with open('.gitconfig', 'w') as gh:
         gh.writelines(
             [
-                '[user]'\n,
+                '[user]\n',
                 'name = poi\n',
                 f'email = {email}'
             ]
